3f846ac8c8549089eab4b854d2f8d1043aba680b
kganitis <ganitiskostas@gmail.com>
2024-04-17 21:54:14 +0300

    Added goal values.

--- DietPlanner/Goal.cs
+++ DietPlanner/Goal.cs
@@ -11,5 +11,15 @@ namespace DietPlanner
         public static readonly string GAIN_WEIGHT = "Αύξηση βάρους";
         public static readonly string LOSE_WEIGHT = "Μείωση βάρους";
         public static readonly string MAINTAIN_WEIGHT = "Διατήρηση βάρους";
+
+        public static int GetValue(string goal)
+        {
+            if (goal == GAIN_WEIGHT)
+                return 1;
+            else if (goal == LOSE_WEIGHT)
+                return -1;
+            else
+                return 0;
+        }
     }
 }
